3bec776d4cea5b291517dbe545b25cd61f4274ac
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-04-07 15:26:18 +0300

    modal hook

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,46 +1,40 @@
 import css from './Modal.module.css';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 
-class Modal extends Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-    currentImageUrl: PropTypes.string.isRequired,
-  };
+const Modal = ({ currentImageUrl, onClose }) => {
+  useEffect(() => {
+    const handlePressESC = e => {
+      console.log('object :>> ', e.keyCode);
+      if (e.keyCode === 27) {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handlePressESC);
 
-  state = {};
+    return () => {
+      window.removeEventListener('keydown', handlePressESC);
+    };
+  }, [onClose]);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handlePressESC);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handlePressESC);
-  }
-
-  handlePressESC = e => {
-    console.log('object :>> ', e.keyCode);
-    if (e.keyCode === 27) {
-      this.props.onClose();
-    }
-  };
-
-  handleBackdropClick = event => {
+  const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    console.log('this.props :>> ', this.props);
-    return (
-      <div className={css.Overlay} onClick={this.handleBackdropClick}>
-        <div className={css.Modal}>
-          <img src={this.props.currentImageUrl} alt="imge" />
-        </div>
+  return (
+    <div className={css.Overlay} onClick={handleBackdropClick}>
+      <div className={css.Modal}>
+        <img src={currentImageUrl} alt="imge" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Modal;
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  currentImageUrl: PropTypes.string.isRequired,
+};
