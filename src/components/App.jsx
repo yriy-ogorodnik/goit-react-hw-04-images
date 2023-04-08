e7a59cdca3f2fa8bd5ce10b3295098c9d6c9d4b0
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-04-08 23:27:26 +0300

    ss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,93 +1,83 @@
-import { Component } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { useState, useEffect } from 'react';
 import Searchbar from 'components/Searchbar/Searchbar';
 import * as API from 'components/Api/Api';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import Button from 'components/Button/Button';
 import Loader from 'components/Loader/Loader';
 import Modal from 'components/Modal/Modal';
-export class App extends Component {
-  state = {
-    searchText: '',
-    images: null,
-    error: null,
-    isLoading: false,
-    page: 1,
-    showModal: false,
-    currentImageUrl: null,
-  };
+
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [images, setImages] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [currentImageUrl, setcurrentImageUrl] = useState(null);
 
   // викликаємо по кліку на пошук. перезаписуємо стейт
-  createSearchText = searchText => {
-    this.setState({ searchText });
+  const createSearchText = searchText => {
+    setSearchText(searchText);
   };
+  useEffect(() => {
+    if (searchText) {
+      setIsLoading(isLoading => !isLoading);
 
-  // метод, который вызывается при обновлении и перерисовке компоненты.
-  // Цей метод не викликається під час першого рендеру
-  componentDidUpdate(prevProps, prevState) {
-    const searchText = this.state.searchText.trim();
-    if (prevState.searchText !== searchText && searchText) {
-      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-      API.fetchImages(searchText)
+      API.fetchImages(searchText, page)
         .then(({ hits, totalHits }) => {
-          this.setState({ images: hits });
+          if (!images) {
+            setImages(hits);
+            toast.success('success');
+          }
+          setImages([...images, ...hits]);
         })
-        .catch(error => this.setState({ error }))
-        .finally(() =>
-          this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-        );
+        .catch(error => toast.error(error.message))
+        .finally(() => setIsLoading(isLoading => !isLoading));
     }
+    //   // __________load more________
+    // if (page !== 1) {
+    //   setIsLoading(isLoading => !isLoading);
+    //   API.fetchImages(searchText, page)
+    //     .then(({ hits }) => {
+    //       setImages([...images, ...hits]);
+    //     })
+    //     .catch(err => toast.error(err.message))
+    //     .finally(() => setIsLoading(isLoading => !isLoading));
+    // }
+  }, [searchText, page, images]);
 
-    // __________load more________
-    if (prevState.page !== this.state.page && this.state.page !== 1) {
-      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-      API.fetchImages(searchText, this.state.page)
-        .then(({ hits }) => {
-          this.setState({ images: [...this.state.images, ...hits] });
-        })
-        .catch(error => this.setState({ error }))
-        .finally(() =>
-          this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-        );
-    }
-  }
-
-  onNextFetch = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
+  const onNextFetch = () => {
+    setPage(page => page + 1);
+    console.log(page);
   };
 
-  openModal = e => {
+  const openModal = e => {
     const currentImageUrl = e.target.dataset.large;
 
     if (e.target.nodeName === 'IMG') {
-      this.setState(({ showModal }) => ({
-        showModal: !showModal,
-        currentImageUrl: currentImageUrl,
-      }));
+      setShowModal(prev => !prev);
+      setcurrentImageUrl(currentImageUrl);
     }
   };
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+
+  const toggleModal = () => {
+    setShowModal(prev => !prev);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <Searchbar createSearchText={this.createSearchText} />
-        {this.state.isLoading && <Loader />}
-        {this.state.error && `${this.state.error}`}
-        {this.state.images && (
-          <ImageGallery images={this.state.images} openModal={this.openModal} />
-        )}
-        {this.state.images && <Button onNextFetch={this.onNextFetch} />}
-        {this.state.showModal && (
-          <Modal
-            onClose={this.toggleModal}
-            currentImageUrl={this.state.currentImageUrl}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Searchbar createSearchText={createSearchText} />
+      {isLoading && <Loader />}
+
+      {images && <ImageGallery images={images} openModal={openModal} />}
+      {images && <Button onNextFetch={onNextFetch} />}
+      {showModal && (
+        <Modal onClose={toggleModal} currentImageUrl={currentImageUrl} />
+      )}
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
