b91d891aea1888840339620f8d61675bcca80f30
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-04-10 01:16:55 +0300

    db

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,11 +2,15 @@ import PropTypes from 'prop-types';
 
 function ImageGalleryItem({ image, openModal }) {
   return (
-    <li className="gallery-item ImageGalleryItem" id={image.id} onClick={openModal}>
+    <li
+      className="gallery-item ImageGalleryItem"
+      id={image.id}
+      onClick={openModal}
+    >
       <img
         src={image.smallImage}
         className="ImageGalleryItem-image "
-        alt={image.dedscription}
+        alt={image.description}
         name={image.largeImage}
         data-large={image.largeImage}
       />
@@ -16,12 +20,12 @@ function ImageGalleryItem({ image, openModal }) {
 
 export default ImageGalleryItem;
 
-// ImageGalleryItem.propTypes = {
-//   image: PropTypes.shape({
-//     webformatURL: PropTypes.string.isRequired,
-//     tags: PropTypes.string.isRequired,
-//     largeImageURL: PropTypes.string.isRequired,
-//   }).isRequired,
+ImageGalleryItem.propTypes = {
+  image: PropTypes.shape({
+    smallImage: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    largeImage: PropTypes.string.isRequired,
+  }).isRequired,
 
-//   openModal: PropTypes.func.isRequired,
-// };
+  openModal: PropTypes.func.isRequired,
+};
